3ac5ac305e35186d052ba04854191a598126cd62
Hritik Rohilla <hritik.rohilla@ambibuzz.com>
2025-11-10 16:34:35 +0530

    Changes MCP initialization code
    Signed-off-by: Hritik Rohilla <hritik.rohilla@ambibuzz.com>

--- ampower_jive/mcp/config/clients.py
+++ ampower_jive/mcp/config/clients.py
@@ -1,11 +1,6 @@
 # Copyright (c) 2025, Ambibuzz Technologies LLP and contributors
 # For license information, please see license.txt
 
-
-import os
 from fastmcp import FastMCP
 
-MCP_HOST = os.environ.get("MCP_HOST", "your-custom-fallback-host")
-MCP_PORT = os.environ.get("MCP_PORT", "your-custom-fallback-port")
-
-mcp = FastMCP("Frappe MCP Server", port=MCP_PORT, host=MCP_HOST, stateless_http=True)
+mcp = FastMCP("Frappe MCP Server")
